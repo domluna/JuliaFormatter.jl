de66dd44a4c6235746a07b4cc904f75648535506
Dominique Luna <dluna132@gmail.com>
2019-12-15 20:39:20 -0500

    skip formatting

--- docs/make.jl
+++ docs/make.jl
@@ -8,6 +8,7 @@ makedocs(
         "Introduction" => "index.md",
         "How It Works" => "how_it_works.md",
         "Code Style" => "style.md",
+        "Skipping Formatting" => "skipping_formatting.md",
         "Syntax Transforms" => "transforms.md",
     ],
 )
