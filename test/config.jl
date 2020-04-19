687dd7d501d2273a34f1d149cc2c8572183e5827
Shuhei Kadowaki <aviatesk@gmail.com>
2020-04-19 13:06:45 +0900

    update testcases and documentation

--- test/config.jl
+++ test/config.jl
@@ -1,96 +1,94 @@
 @testset "configuration" begin
-    before = "begin rand() end\n"
-    config = "indent = 2"
-    after4 = "begin\n    rand()\nend\n"
-    after2 = "begin\n  rand()\nend\n"
-
-    # test a case when separate file paths are given
-    # ├─ sandbox1
-    # │  └─ code1.jl (before -> after4)
-    # └─ sandbox2
-    #    ├─ .JuliaFormatter.toml (config2)
-    #    └─ code2.jl (before -> after2)
-    sandbox1_dir = mktempdir(@__DIR__)
-    sandbox2_dir = mktempdir(@__DIR__)
-    try
-        code1_path = joinpath(sandbox1_dir, "code1.jl")
-        config2_path = joinpath(sandbox2_dir, CONFIG_FILE_NAME)
-        code2_path = joinpath(sandbox2_dir, "code2.jl")
-        open(io -> write(io, before), code1_path, "w")
-        open(io -> write(io, config), config2_path, "w")
-        open(io -> write(io, before), code2_path, "w")
-
-        format((code1_path, code2_path))
-        @test read(code1_path, String) == after4
-        @test read(code2_path, String) == after2
-    finally
-        rm(sandbox1_dir; recursive = true)
-        rm(sandbox2_dir; recursive = true)
-    end
-end
-
-@testset "configuration directory walk" begin
-    before = "begin rand() end\n"
-    after4 = "begin\n    rand()\nend\n"
-    after2 = "begin\n  rand()\nend\n"
-    config4 = "indent = 4"
     config2 = "indent = 2"
+    config4 = "indent = 4"
+    before = "begin rand() end\n"
+    after2 = "begin\n  rand()\nend\n"
+    after4 = "begin\n    rand()\nend\n"
 
     # test basic configuration case
     # test_basic_config
     # ├─ .JuliaFormatter.toml (config2)
     # └─ code.jl (before -> after2)
     sandbox_dir = joinpath(@__DIR__, "test_basic_config")
+    mkdir(sandbox_dir)
     try
-        mkdir(sandbox_dir)
         config_path = joinpath(sandbox_dir, CONFIG_FILE_NAME)
         code_path = joinpath(sandbox_dir, "code.jl")
         open(io -> write(io, config2), config_path, "w")
         open(io -> write(io, before), code_path, "w")
 
-        format(sandbox_dir)
+        format(code_path)
         @test read(code_path, String) == after2
     finally
         rm(sandbox_dir; recursive = true)
     end
 
-    # test configs only applies to a sub directory
-    # test_subdir_config
-    # ├─ code.jl (before -> after4)
+    # test upward config search
+    # test_search
+    # ├─ .JuliaFormatter.toml (config2)
     # └─ sub
-    #    ├─ .JuliaFormatter.toml (config2)
-    #    └─ sub_code.jl (before -> after2)
-    sandbox_dir = joinpath(@__DIR__, "test_subdir_config")
+    #    ├─ sub_code.jl (before -> after2)
+    #    └─ subsub
+    #       └─ subsub_code.jl (before -> after2)
+    sandbox_dir = joinpath(@__DIR__, "test_search")
+    mkdir(sandbox_dir)
     try
-        mkdir(sandbox_dir)
+        config_path = joinpath(sandbox_dir, CONFIG_FILE_NAME)
         sub_dir = joinpath(sandbox_dir, "sub")
         mkdir(sub_dir)
-        code_path = joinpath(sandbox_dir, "code.jl")
-        sub_config_path = joinpath(sub_dir, CONFIG_FILE_NAME)
         sub_code_path = joinpath(sub_dir, "sub_code.jl")
+        subsub_dir = joinpath(sub_dir, "sub")
+        mkdir(subsub_dir)
+        subsub_code_path = joinpath(subsub_dir, "sub_code.jl")
+        open(io -> write(io, config2), config_path, "w")
+        open(io -> write(io, before), sub_code_path, "w")
+        open(io -> write(io, before), subsub_code_path, "w")
+
+        format(sub_code_path)
+        @test read(sub_code_path, String) == after2
+        format(subsub_code_path)
+        @test read(subsub_code_path, String) == after2
+    finally
+        rm(sandbox_dir; recursive = true)
+    end
+
+    # test basic directory walk
+    # test_basic_walk
+    # ├─ .JuliaFormatter.toml (config2)
+    # ├─ code.jl (before -> after2)
+    # └─ sub
+    #    └─ sub_code.jl (before -> after2)
+    sandbox_dir = joinpath(@__DIR__, "test_basic_walk")
+    mkdir(sandbox_dir)
+    try
+        config_path = joinpath(sandbox_dir, CONFIG_FILE_NAME)
+        code_path = joinpath(sandbox_dir, "code.jl")
+        sub_dir = joinpath(sandbox_dir, "sub")
+        mkdir(sub_dir)
+        sub_code_path = joinpath(sub_dir, "sub_code.jl")
+        open(io -> write(io, config2), config_path, "w")
         open(io -> write(io, before), code_path, "w")
-        open(io -> write(io, config2), sub_config_path, "w")
         open(io -> write(io, before), sub_code_path, "w")
 
         format(sandbox_dir)
-        @test read(code_path, String) == after4
+        @test read(code_path, String) == after2
         @test read(sub_code_path, String) == after2
     finally
         rm(sandbox_dir; recursive = true)
     end
 
-    # test configs defined in nested way
+    # test directory walk with nested configs
     # test_nested_config
-    # ├─ .JuliaFormatter.toml (config2)
-    # ├─ code.jl (before -> after2)
+    # ├─ .JuliaFormatter.toml (config4)
+    # ├─ code.jl (before -> after4)
     # ├─ sub1
-    # │  ├─ .JuliaFormatter.toml (config4)
-    # │  └─ sub_code1.jl (before -> after4)
+    # │  ├─ .JuliaFormatter.toml (config2)
+    # │  └─ sub_code1.jl (before -> after2)
     # └─ sub2
-    #    └─ sub_code2.jl (before -> after2)
+    #    └─ sub_code2.jl (before -> after4)
     sandbox_dir = joinpath(@__DIR__, "test_nested_config")
+    mkdir(sandbox_dir)
     try
-        mkdir(sandbox_dir)
         sub1_dir = joinpath(sandbox_dir, "sub1")
         sub2_dir = joinpath(sandbox_dir, "sub2")
         mkdir(sub1_dir)
