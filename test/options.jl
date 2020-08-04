9c66c96288243ba0ecf5e2134d090b21a4ac5bbe
Dominique Luna <dluna132@gmail.com>
2020-08-04 15:26:05 -0400

    tests

--- test/options.jl
+++ test/options.jl
@@ -719,4 +719,56 @@
         """
         @test fmt(short, format_docstrings = true) == short_formatted
     end
+
+    @testset "align struct fields" begin
+        str_ = """
+        struct Foo
+            a::T
+            longfieldname::B
+        end"""
+        str = """
+        struct Foo
+            a             :: T
+            longfieldname :: B
+        end"""
+        @test fmt(str_, align_struct_fields = true) == str
+        @test fmt(str, align_struct_fields = false) == str_
+
+        str_ = """
+        Base.@kwdef struct Options
+            indent_size::Int = 4
+            max_margin::Int = 92
+            always_for_in::Bool = false
+            whitespace_typedefs::Bool = false
+            whitespace_ops_in_indices::Bool = false
+            remove_extra_newlines::Bool = false
+            import_to_using::Bool = false
+            pipe_to_function_call::Bool = false
+            short_to_long_function_def::Bool = false
+            always_use_return::Bool = false
+            whitespace_in_kwargs::Bool = true
+            annotate_untyped_fields_with_any::Bool = true
+            format_docstrings::Bool = false
+            align_struct_fields::Bool = false
+        end"""
+        str = """
+        Base.@kwdef struct Options
+            indent_size::Int                       = 4
+            max_margin::Int                        = 92
+            always_for_in::Bool                    = false
+            whitespace_typedefs::Bool              = false
+            whitespace_ops_in_indices::Bool        = false
+            remove_extra_newlines::Bool            = false
+            import_to_using::Bool                  = false
+            pipe_to_function_call::Bool            = false
+            short_to_long_function_def::Bool       = false
+            always_use_return::Bool                = false
+            whitespace_in_kwargs::Bool             = true
+            annotate_untyped_fields_with_any::Bool = true
+            format_docstrings::Bool                = false
+            align_struct_fields::Bool              = false
+        end"""
+        @test fmt(str_, align_struct_fields = true) == str
+        @test fmt(str, align_struct_fields = false) == str_
+    end
 end
