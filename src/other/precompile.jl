b18998064d6b7147f8f4d5344a8418408ede4c59
Dominique Luna <dluna132@gmail.com>
2021-06-29 20:21:30 -0400

    precompile header

--- src/other/precompile.jl
+++ src/other/precompile.jl
@@ -1,7 +1,9 @@
 #! format: off
 function _precompile_()
+    ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
     # pretty
     Base.precompile(Tuple{typeof(pretty),DefaultStyle,CSTParser.EXPR,State})
     Base.precompile(Tuple{typeof(pretty),YASStyle,CSTParser.EXPR,State})
     Base.precompile(Tuple{typeof(pretty),BlueStyle,CSTParser.EXPR,State})
+
 end
