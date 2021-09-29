015e85f6de4895335e23043f2d254aed2ad1cd3d
Dominique Luna <dluna132@gmail.com>
2021-09-28 20:48:21 -0400

    fmt

--- test/format_repo.jl
+++ test/format_repo.jl
@@ -2,14 +2,14 @@
     sandbox_dir = joinpath(tempdir(), "format_repo_test")
     mkdir(sandbox_dir)
     try
-        cp(@__DIR__, sandbox_dir; force=true)
+        cp(@__DIR__, sandbox_dir; force = true)
 
         # initial format
         format(sandbox_dir)
 
         # follow up formats should be the same
-        @test format(sandbox_dir, ignore_maximum_width=true) == true
-        @test format(sandbox_dir, ignore_maximum_width=true, margin=10_000) == true
+        @test format(sandbox_dir, ignore_maximum_width = true) == true
+        @test format(sandbox_dir, ignore_maximum_width = true, margin = 10_000) == true
     finally
         rm(sandbox_dir; recursive = true)
     end
