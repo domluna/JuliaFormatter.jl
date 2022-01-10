27c14f6a4c303b044aa06b4647bdc10b28055a77
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2022-01-10 17:20:29 -0500

    docstring

--- src/styles/yas/pretty.jl
+++ src/styles/yas/pretty.jl
@@ -13,6 +13,7 @@ Configurable options with different defaults to [`DefaultStyle`](@ref) are:
 - `short_to_long_function_def` = true
 - `always_use_return` = true
 - `whitespace_in_kwargs` = false
+- `join_lines_based_on_source` = true
 """
 struct YASStyle <: AbstractStyle
     innerstyle::Union{Nothing,AbstractStyle}
