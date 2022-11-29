de44c02f53c2a9113e3da16ba6d2f848c6396392
Dominique Luna <dluna132@gmail.com>
2022-11-29 15:54:37 -0500

    Limit usage of InverseTrailingSemicolon  (#661)
    * Limit usage of InverseTrailingSemicolon
    
    Depending on the types inside a vector
    
    ```
    [
      ...
    ]
    ```
    
    Having removing or adding the semicolon can change the type entirely.
    
    ```
    julia> [
           [1,0];
           ]
    2-element Vector{Int64}:
     1
     0
    
    julia> [
           [1,0]
           ]
    1-element Vector{Vector{Int64}}:
     [1, 0]
    ```
    
    So we should no longer do that.
    
    fix #655
    
    * additional tests

--- test/issues.jl
+++ test/issues.jl
@@ -1486,4 +1486,22 @@
         """
         @test fmt(s, 4, 92, align_assignment = true) == s
     end
+
+    @testset "655" begin
+        s = """
+        [
+          a;
+        ]
+        """
+        @test fmt(s, 2, 92, join_lines_based_on_source = true) == s
+        @test fmt(s, 2, 1) == s
+        s = """
+        [
+          a
+        ]
+        """
+        @test fmt(s, 2, 92, join_lines_based_on_source = true, trailing_comma = nothing) ==
+              s
+        @test fmt(s, 2, 1, trailing_comma = nothing) == s
+    end
 end
