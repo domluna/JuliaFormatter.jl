ed0b201c5055622a06bf5d5ffc93e861402b5b47
Chris Elrod <elrodc@gmail.com>
2023-01-20 16:08:51 -0500

    Use SnoopPrecompile.

--- src/other/precompile.jl
+++ src/other/precompile.jl
@@ -7,3 +7,10 @@ function _precompile_()
     Base.precompile(Tuple{typeof(pretty),BlueStyle,CSTParser.EXPR,State})
     Base.precompile(Tuple{typeof(pretty),MinimalStyle,CSTParser.EXPR,State})
 end
+using SnoopPrecompile
+@precompile_setup begin
+    dir = joinpath(@__DIR__,"../..")
+    @precompile_all_calls begin
+        format(dir)
+    end
+end
