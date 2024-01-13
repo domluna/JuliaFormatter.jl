5fa778842f5d618456ff68e267f48a8fb2b7ac5e
David Widmann <devmotion@users.noreply.github.com>
2024-01-13 02:07:43 +0100

    Handle empty docstrings

--- src/styles/default/pretty.jl
+++ src/styles/default/pretty.jl
@@ -284,63 +284,69 @@ end
 function format_docstring(style::AbstractStyle, state::State, text::AbstractString)
     state_indent = state.indent
     start_boundary = findfirst(!=('"'), text)
-    end_boundary = findlast(!=('"'), text)
-    # first, we need to remove any user indent
-    # only some lines will "count" towards increasing the user indent
-    # start at a very big guess
-    user_indent = typemax(Int)
-    user_indented = text[start_boundary:end_boundary]
-    deindented = IOBuffer()
-    user_lines = split(user_indented, '\n')
-    for (index, line) in enumerate(user_lines)
-        # the first line doesn't count
-        if index != 1
-            num_spaces = 0
-            for c in line
-                isspace(c) || break
-                num_spaces += 1
-            end
-            # if the line is only spaces, it only counts if it is the last line
-            if num_spaces < length(line) || index == length(user_lines)
-                user_indent = min(user_indent, num_spaces)
-            end
-        end
-    end
-    deindented_string =
-    # if there are no lines at all, or if the user indent is zero, we don't have to change anything
-        if user_indent == typemax(Int) || user_indent == 0
-            user_indented
-        else
-            # else, deindent non-first lines
-            first_line = true
-            for line in split(user_indented, '\n')
-                if first_line
-                    first_line = false
-                    write(deindented, line)
-                else
-                    write(deindented, '\n')
-                    write(deindented, chop(line; head = user_indent, tail = 0))
+    # if the docstring is non-empty
+    if start_boundary !== nothing
+        end_boundary = findlast(!=('"'), text)
+        # first, we need to remove any user indent
+        # only some lines will "count" towards increasing the user indent
+        # start at a very big guess
+        user_indent = typemax(Int)
+        user_indented = text[start_boundary:end_boundary]
+        deindented = IOBuffer()
+        user_lines = split(user_indented, '\n')
+        for (index, line) in enumerate(user_lines)
+            # the first line doesn't count
+            if index != 1
+                num_spaces = 0
+                for c in line
+                    isspace(c) || break
+                    num_spaces += 1
+                end
+                # if the line is only spaces, it only counts if it is the last line
+                if num_spaces < length(line) || index == length(user_lines)
+                    user_indent = min(user_indent, num_spaces)
                 end
             end
-            String(take!(deindented))
         end
+        deindented_string =
+        # if there are no lines at all, or if the user indent is zero, we don't have to change anything
+            if user_indent == typemax(Int) || user_indent == 0
+                user_indented
+            else
+                # else, deindent non-first lines
+                first_line = true
+                for line in split(user_indented, '\n')
+                    if first_line
+                        first_line = false
+                        write(deindented, line)
+                    else
+                        write(deindented, '\n')
+                        write(deindented, chop(line; head = user_indent, tail = 0))
+                    end
+                end
+                String(take!(deindented))
+            end
 
-    # then, we format
-    formatted = markdown(
-        enable!(
-            Parser(),
-            [
-                AdmonitionRule(),
-                FootnoteRule(),
-                MathRule(),
-                TableRule(),
-                FrontMatterRule(),
-                FormatRule(style, state.opts),
-            ],
-        )(
-            deindented_string,
-        ),
-    )
+        # then, we format
+        formatted = markdown(
+            enable!(
+                Parser(),
+                [
+                    AdmonitionRule(),
+                    FootnoteRule(),
+                    MathRule(),
+                    TableRule(),
+                    FrontMatterRule(),
+                    FormatRule(style, state.opts),
+                ],
+            )(
+                deindented_string,
+            ),
+        )
+    else
+        # the docstring is empty
+        formatted = ""
+    end
     # Indent all non-first lines to match the current parser indent
     buf = IOBuffer()
     indent = " "^state_indent
