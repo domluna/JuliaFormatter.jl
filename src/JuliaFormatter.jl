e8282bcaebadbec283414174810e92ac3dc03184
Dominique Luna <dluna132@gmail.com>
2024-09-05 20:55:50 -0400

    add warning to skip file if it's a parse error

--- src/JuliaFormatter.jl
+++ src/JuliaFormatter.jl
@@ -240,7 +240,7 @@ function format_text(text::AbstractString, style::AbstractStyle, opts::Options)
         @assert valid_for_in_op(opts.for_in_replacement) "`for_in_replacement` is set to an invalid operator \"$(opts.for_in_replacement)\", valid operators are $(VALID_FOR_IN_OPERATORS). Change it to one of the valid operators and then reformat."
     end
     t = JuliaSyntax.parseall(JuliaSyntax.GreenNode, text)
-    @info "parsed" t
+    # @info "parsed" t
     return format_text(t, style, State(Document(text), opts))
 end
 
@@ -462,6 +462,10 @@ function format(path::AbstractString, options::Configuration)
     try
         return _format_file(path; [Symbol(k) => v for (k, v) in pairs(options)]...)
     catch err
+        if err isa JuliaSyntax.ParseError
+            @warn "Failed to format file $path due to a parsing error, skipping file" error=err
+            return true
+        end
         @info "Error in formatting file $path"
         @debug "formatting failed due to" exception = (err, catch_backtrace())
         return _format_file(path; [Symbol(k) => v for (k, v) in pairs(options)]...)
--- test/format_repo.jl
+++ test/format_repo.jl
@@ -1,6 +1,7 @@
 @testset "Format repo" begin
     try
         sandbox_dir = joinpath(tempdir(), join(rand('a':'z', 40)))
+        @info "formatting in directory" sandbox_dir
         mkdir(sandbox_dir)
         cp(@__DIR__, sandbox_dir; force = true)
 
@@ -12,9 +13,9 @@
         @test format(sandbox_dir, join_lines_based_on_source = true, margin = 10_000) ==
               true
     finally
-        try
-            rm(sandbox_dir; recursive = true)
-        catch
-        end
+        # try
+        #     rm(sandbox_dir; recursive = true)
+        # catch
+        # end
     end
 end
